b7198abf947e11df8b7b6d31853e564e2f7f0c3c
Артём <@DESKTOP-PJ0UIMN>
2023-06-09 21:53:19 +0500

    Добавьте файлы проекта.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backGround = new System.Windows.Forms.Timer(this.components);
+            this.mainPlayer = new System.Windows.Forms.PictureBox();
+            this.Left = new System.Windows.Forms.Timer(this.components);
+            this.Right = new System.Windows.Forms.Timer(this.components);
+            this.Up = new System.Windows.Forms.Timer(this.components);
+            this.Down = new System.Windows.Forms.Timer(this.components);
+            this.BulletsTimer = new System.Windows.Forms.Timer(this.components);
+            this.oppTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelLevel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backGround
+            // 
+            this.backGround.Enabled = true;
+            this.backGround.Interval = 10;
+            this.backGround.Tick += new System.EventHandler(this.backGround_Tick);
+            // 
+            // mainPlayer
+            // 
+            this.mainPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.mainPlayer.Image = global::Game.Properties.Resources.PlayerStay;
+            this.mainPlayer.Location = new System.Drawing.Point(24, 367);
+            this.mainPlayer.Name = "mainPlayer";
+            this.mainPlayer.Size = new System.Drawing.Size(184, 261);
+            this.mainPlayer.TabIndex = 0;
+            this.mainPlayer.TabStop = false;
+            // 
+            // Left
+            // 
+            this.Left.Interval = 10;
+            this.Left.Tick += new System.EventHandler(this.Left_Tick);
+            // 
+            // Right
+            // 
+            this.Right.Interval = 10;
+            this.Right.Tick += new System.EventHandler(this.Right_Tick);
+            // 
+            // Up
+            // 
+            this.Up.Interval = 10;
+            this.Up.Tick += new System.EventHandler(this.Up_Tick);
+            // 
+            // Down
+            // 
+            this.Down.Interval = 10;
+            this.Down.Tick += new System.EventHandler(this.Down_Tick);
+            // 
+            // BulletsTimer
+            // 
+            this.BulletsTimer.Enabled = true;
+            this.BulletsTimer.Interval = 10;
+            this.BulletsTimer.Tick += new System.EventHandler(this.BulletsTimer_Tick);
+            // 
+            // oppTimer
+            // 
+            this.oppTimer.Enabled = true;
+            this.oppTimer.Interval = 10;
+            this.oppTimer.Tick += new System.EventHandler(this.oppTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Std Black", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(849, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 117);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Algerian", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label2.Location = new System.Drawing.Point(4, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 63);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Algerian", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label3.Location = new System.Drawing.Point(4, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 63);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Level:";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.BackColor = System.Drawing.Color.Transparent;
+            this.labelScore.Font = new System.Drawing.Font("Algerian", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.Color.LawnGreen;
+            this.labelScore.Location = new System.Drawing.Point(225, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(95, 63);
+            this.labelScore.TabIndex = 4;
+            this.labelScore.Text = "00";
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.BackColor = System.Drawing.Color.Transparent;
+            this.labelLevel.Font = new System.Drawing.Font("Algerian", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLevel.ForeColor = System.Drawing.Color.LawnGreen;
+            this.labelLevel.Location = new System.Drawing.Point(225, 72);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(96, 63);
+            this.labelLevel.TabIndex = 5;
+            this.labelLevel.Text = "01";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game.Properties.Resources._1644918766_4_phonoteka_org_p_kosmicheskii_fon_dlya_igri_4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.labelLevel);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainPlayer);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer backGround;
+        private System.Windows.Forms.PictureBox mainPlayer;
+        private System.Windows.Forms.Timer Left;
+        private System.Windows.Forms.Timer Right;
+        private System.Windows.Forms.Timer Up;
+        private System.Windows.Forms.Timer Down;
+        private System.Windows.Forms.Timer BulletsTimer;
+        private System.Windows.Forms.Timer oppTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelLevel;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] fog;
+        int backGroundspeed; // скорость тумана
+        Random rnd;
+        int playerSpeed;
+
+        PictureBox[] bullets;
+        int bulletsSpeed;
+
+        int Score;
+        int Level;
+
+        WindowsMediaPlayer shoot;
+        WindowsMediaPlayer GameSong;
+        WindowsMediaPlayer Rip;
+        WindowsMediaPlayer KillPlayer;
+
+        PictureBox[] opponent;
+        int sizeOpponent;
+        int opponentSpeed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backGround_Tick(object sender, EventArgs e)
+        {
+            // смещение тумана
+            for (int i = 0; i < fog.Length; i++)
+            {
+                fog[i].Left += backGroundspeed;
+
+                if (fog[i].Left >= 1280)
+                {
+                    fog[i].Left = fog[i].Height;
+                }
+            }
+
+            for (int i = fog.Length; i < fog.Length; i++)
+            {
+                fog[i].Left += backGroundspeed - 10;
+
+                if (fog[i].Left >= 2000)
+                {
+                    fog[i].Left = fog[i].Left;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            backGroundspeed = 5;
+            fog = new PictureBox[20];
+            rnd = new Random();
+            playerSpeed = 4 ;
+
+            bullets = new PictureBox[1];
+            bulletsSpeed = 50;
+
+            shoot = new WindowsMediaPlayer();
+            shoot.URL = "songs\\shoot.mp3";
+            shoot.settings.volume = 5;
+
+            Rip = new WindowsMediaPlayer();
+            Rip.URL = "songs\\Rip.mp3";
+            Rip.settings.volume = 15;
+
+            KillPlayer = new WindowsMediaPlayer();
+            KillPlayer.URL = "songs\\KillPlayer.mp3";
+            KillPlayer.settings.volume = 15;
+
+            GameSong = new WindowsMediaPlayer();
+            GameSong.URL = "songs\\songGame.mp3";
+            GameSong.settings.setMode("loop", true);
+            GameSong.settings.volume = 20;
+
+            opponent = new PictureBox[5];
+            int sizeOpponent = rnd.Next(60, 80);
+            opponentSpeed = 5;
+
+            Score = 0;
+            Level = 1;
+
+
+
+            Image easyOpponent = Image.FromFile("assets\\opp.png");
+
+            for(int i = 0; i < opponent.Length; i++)
+            {
+                opponent[i] = new PictureBox();
+                opponent[i].Size = new Size(sizeOpponent, sizeOpponent);
+                opponent[i].SizeMode = PictureBoxSizeMode.Zoom;
+                opponent[i].BackColor = Color.Transparent;
+                opponent[i].Image = easyOpponent;
+                opponent[i].Location = new Point((i + 1) * rnd.Next(90, 160) + 600, rnd.Next(150, 300));
+
+                this.Controls.Add(opponent[i]);
+            }
+
+            for( int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i] = new PictureBox();
+                bullets[i].BorderStyle = BorderStyle.None;
+                bullets[i].Size = new Size(30, 10);
+                bullets[i].BackColor = Color.Red;
+
+                this.Controls.Add(bullets[i]);
+
+            }
+
+            // инициализация тумана
+
+            for (int i = 0; i < fog.Length; i++)
+            {
+                fog[i] = new PictureBox();
+                fog[i].BorderStyle = BorderStyle.None;
+                
+                fog[i].Location = new Point(rnd.Next(-4000, 2000), rnd.Next(150, 300)); // кординаты рандомного появления тумана
+                
+                // 2 вида тумана, четный одного размера, нечетный другого
+                if (i % 2 == 1)
+                {
+                    // размер тумана в диапозоне
+                    fog[i].Size = new Size(rnd.Next(100, 255), rnd.Next(30, 70));
+                    
+                    fog[i].BackColor = Color.FromArgb(rnd.Next(50, 125), 255, 200, 200); // рандомная прозрачность тумана
+                }
+                else
+                {
+                    fog[i].Size = new Size(100, 30);
+                    fog[i].BackColor = Color.FromArgb(rnd.Next(50, 125), 255, 205, 205);
+                }
+                this.Controls.Add(fog[i]);
+            }
+
+            GameSong.controls.play();
+        }
+
+        private void Left_Tick(object sender, EventArgs e)
+        {
+            if(mainPlayer.Left > 10)
+            {
+                mainPlayer.Left -= playerSpeed; 
+            }
+        }
+
+        private void Right_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Left < 1150)
+            {
+                mainPlayer.Left += playerSpeed;
+            }
+        }
+
+        private void Up_Tick(object sender, EventArgs e)
+        {
+            mainPlayer.Top -= playerSpeed;
+        }
+
+        private void Down_Tick(object sender, EventArgs e)
+        {
+            mainPlayer.Top += playerSpeed;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            mainPlayer.Image = Properties.Resources.PlayerStay;
+
+            if (e.KeyCode == Keys.Left)
+            {
+                Left.Start();
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                Right.Start();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                Up.Start();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                Down.Start();
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                shoot.controls.play();
+                for(int i = 0; i < bullets.Length; i++)
+                {
+                    Intersect();
+                    if (bullets[i].Left > 1280)
+                    {
+                        bullets[i].Location = new Point(mainPlayer.Location.X + 10 + i *10, mainPlayer.Location.Y + 58);
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            mainPlayer.Image = Properties.Resources.PlayerStay;
+
+            Left.Stop();
+            Right.Stop();
+            Up.Stop();
+            Down.Stop();
+        }
+
+        private void BulletsTimer_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i].Left += bulletsSpeed;
+            }
+        }
+
+        private void oppTimer_Tick(object sender, EventArgs e)
+        {
+            MoveOpp(opponent, opponentSpeed);
+        }
+
+        private void MoveOpp(PictureBox[] opponent, int speed)
+        {
+            for (int i = 0; i < opponent.Length; i++)
+            {
+                opponent[i].Left -= speed + (int)(Math.Sin(opponent[i].Left) * Math.PI / 180);
+
+                Intersect();
+
+                if (opponent[i].Left < 0)
+                {
+                    int sizeOpponent = rnd.Next(60, 90);
+                    opponent[i].Size = new Size(sizeOpponent, sizeOpponent);
+                    opponent[i].Location = new Point((i + 1) * rnd.Next(150, 250) + 600, rnd.Next(150, 400));
+
+                }
+            }
+        }
+
+        private void Intersect()
+        {
+            for(int i = 0;i < opponent.Length;i++)
+            {
+                if (bullets[0].Bounds.IntersectsWith(opponent[i].Bounds))
+                {
+                    Rip.controls.play();
+
+                    Score += 1;
+                    labelScore.Text = (Score <10) ? "0" + Score.ToString() : Score.ToString();
+
+                    if(Score % 10 == 0)
+                    {
+                        Level += 1;
+                        labelLevel.Text = (Level < 10) ? "0" + Level.ToString() : Level.ToString();
+
+                        if(opponentSpeed <= 3)
+                        {
+                            opponentSpeed++;
+                        }
+
+                        if(Level == 10)
+                        {
+                            GameOver("Epic Power!");
+                        }
+                    }
+
+                    opponent[i].Location = new Point((i + 1) * rnd.Next(150, 250) + 800, rnd.Next(150, 300));
+                    bullets[0].Location = new Point(1000, mainPlayer.Location.Y);
+                }
+
+                if (mainPlayer.Bounds.IntersectsWith(opponent[i].Bounds))
+                {
+                    mainPlayer.Visible = false;
+
+                    GameOver("Game Over");
+                }
+            }
+        }
+        
+        private void GameOver(string str)
+        {
+            label1.Text = str;
+            label1.Location = new Point(400, 40);
+            label1.Visible = true;
+
+            GameSong.controls.stop();
+            oppTimer.Stop();
+            KillPlayer.controls.play();
+
+        }
+    }
+}
